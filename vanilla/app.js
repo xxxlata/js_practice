5c9606745274370bb31ea1c9f6533833e52e0d9a
bigtoe <xxxlata@naver.com>
2021-09-21 21:34:36 +0900

    vanilla

---
+++ vanilla/app.js
@@ -0,0 +1,28 @@
+//const 업데이트 불가능한 변수 생성 , let 업데이트 가능 변수 생성
+/*
+const a = 5;
+const b = 2;
+let myName = "juha";
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello" + myName);
+*/
+
+//null 아무것도 없음 의미 false랑 다름 
+/*
+const amIFat = null;
+console.log(amIFat);
+*/
+
+const mon = "mon";
+const tue = "tue";
+const wed = "wed";
+const fri = "fri";
+const sat = "sat";
+const sun = "sun";
+
+const
+
