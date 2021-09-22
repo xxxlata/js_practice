f93ce4f5d14a3d84706e53e098e52f0b35755d0c
bigtoe <xxxlata@naver.com>
2021-09-22 22:06:23 +0900

    vanilla2

--- vanilla/app.js
+++ vanilla/app.js
@@ -17,12 +17,57 @@ const amIFat = null;
 console.log(amIFat);
 */
 
-const mon = "mon";
-const tue = "tue";
-const wed = "wed";
-const fri = "fri";
-const sat = "sat";
-const sun = "sun";
+function sayhello(nameOfPerson,age){
+    console.log("Hello my name is"+nameOfPerson + "and i am" + age);
+}
 
-const
+sayhello('nico',10);
+sayhello('dal',23);
+sayhello('juha',25);
 
+function plus(a,b){
+    console.log(a+b);
+}
+function divide(a,b){
+    console.log(a / b);
+}
+plus(8,60);
+divide(34,54);
+
+const player = {
+    name: "juha",
+    sayhello: function(otherPersonsName){
+        console.log("helo!" + otherPersonsName);
+    },
+};
+
+player.sayhello("lynn");
+player.sayhello("juha");
+
+const age = parseInt(prompt("How old are you?"));
+
+if(isNaN(age)) {
+    console.log("please write a number");
+    
+}else if (age < 18) {
+    console.log("you are too young.");
+}else {
+    console.log("you can drink");
+}
+
+const age = parseInt(prompt("How old are you?"));
+
+if(isNaN(age) || age < 0) {
+    console.log("please write a number");
+    
+}else if (age < 18) {
+    console.log("you are too young.");
+}else if (age >= 18 && age <= 50){
+    console.log("you can drink");
+}else if(age > 50 && age <=80){
+    console.log("you should exercise");
+    
+}else if (age >80){
+    console.log("you can do whatever you want.");
+
+}
