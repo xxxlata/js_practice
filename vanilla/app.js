0faf984775dfb25c4f7060b38619fca2b5939732
bigtoe <xxxlata@naver.com>
2021-09-25 12:25:42 +0900

    vanilla

--- vanilla/app.js
+++ vanilla/app.js
@@ -6,7 +6,7 @@ const title = document.querySelector("#title");
 
 title.innerText ="Got you!";
 */
-
+/*
 //click event
 const title = document.querySelector("div.hello:first-child h1");
 
@@ -40,3 +40,30 @@ window.addEventListener("resize", handleWindowResize);
 window.addEventListener("copy", handleWindowCopy);
 window.addEventListener("offline",handleWindowOffline);
 window.addEventListener("online",handleWindowOnline);
+
+*/
+/*
+const h1 = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick() {
+    const currentColor = h1.style.color;
+    let newColor;
+    if (currentColor === "blue"){
+        newColor= "tomato";
+    }else {
+        newColor= "blue";    
+    }
+    h1.style.color = newColor;
+}
+
+h1.addEventListener("click", handleTitleClick);
+
+//easy way use toggle with css
+*/
+const h1 = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick() {
+    h1.classList.toggle("clicked");    
+}
+
+h1.addEventListener("click", handleTitleClick);
